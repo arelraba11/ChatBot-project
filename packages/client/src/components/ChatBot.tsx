ab0249972c80afdf8b7e9297e11b9caa36f98aca
Arel Raba <arelraba11@gmail.com>
2025-12-10 18:17:29 +0200

    start design frontend side

---
+++ packages/client/src/components/ChatBot.tsx
@@ -0,0 +1,81 @@
+import { useForm } from 'react-hook-form';
+import { Button } from './ui/button';
+import { FaArrowUp } from 'react-icons/fa';
+import axios from 'axios';
+import { useRef, useState } from 'react';
+import ReactMarkDown from 'react-markdown';
+
+type FormData = {
+   prompt: string;
+};
+
+type ChatResponse = {
+   message: string;
+};
+
+type Message = {
+   content: string;
+   role: 'user' | 'bot';
+};
+
+const ChatBot = () => {
+   const [messages, setMessages] = useState<Message[]>([]);
+   const conversationId = useRef(crypto.randomUUID());
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+   const onSubmit = async ({ prompt }: FormData) => {
+      setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
+
+      reset();
+
+      const { data } = await axios.post<ChatResponse>('/api/chat', {
+         prompt,
+         conversationId: conversationId.current,
+      });
+      setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
+   };
+
+   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {};
+
+   return (
+      <div>
+         <div className="flex flex-col gap-3 mb-10">
+            {messages.map((message, index) => (
+               <p
+                  key={index}
+                  className={`px-3 py-1 rounded-xl ${
+                     message.role === 'user'
+                        ? ' text-black self-end bg-blue-600'
+                        : ' text-black bg-gray-100'
+                  }`}
+               >
+                  <ReactMarkDown>{message.content}</ReactMarkDown>
+               </p>
+            ))}
+         </div>
+         <form
+            onSubmit={handleSubmit(onSubmit)}
+            onKeyDown={onKeyDown}
+            className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+         >
+            <textarea
+               {...register('prompt', {
+                  required: true,
+                  validate: (data) => data.trim().length > 0,
+               })}
+               className="w-full border-0 focus:outline-0 resize-none"
+               placeholder="Ask anything"
+               maxLength={1000}
+            />
+            <Button
+               disabled={!formState.isValid}
+               className="rounded-full w-9 h-9"
+            >
+               <FaArrowUp />
+            </Button>
+         </form>
+      </div>
+   );
+};
+
+export default ChatBot;
