7bb29cb207ca45f176b163a8789bae3a14436673
Arel Raba <arelraba11@gmail.com>
2025-12-25 13:51:10 +0200

    Setup Prisma

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import { chatController } from './controllers/chat.controller';
+import { never } from 'zod';
+import { PrismaClient } from './generated/prisma';
 
 const router = express.Router();
 
@@ -13,5 +15,22 @@ router.get('/api/hello', (req: Request, res: Response) => {
 });
 
 router.post('/api/chat', chatController.sendMessage);
+
+router.get('/api/products/:id/reviews', async (req: Request, res: Response) => {
+   const prisma = new PrismaClient();
+   const productId = Number(req.params.id);
+
+   if (isNaN(productId)) {
+      res.status(400).json({ error: 'Invaild product ID.' });
+      return;
+   }
+
+   const reviews = await prisma.review.findMany({
+      where: { productId },
+      orderBy: { createdAt: 'desc' },
+   });
+
+   res.json(reviews);
+});
 
 export default router;
